70c3607d2316ebf03902685fc9566600c1553424
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2024-08-19 18:02:12 -0600

    got modal working however, might not be best solution for this project

--- src/components/MousePosition.jsx
+++ src/components/MousePosition.jsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import Modal from "./Modal";
 
 export default function MousePosition() {
   const [x, setX] = useState("");
   const [y, sety] = useState("");
-
+  const dialogRef = useRef(null);
   const toggle = (e) => {
     setX(e.nativeEvent.offsetX);
     sety(e.nativeEvent.offsetY);
   };
 
-  const modal = () => {};
-
+  const modal = () => {
+    console.log(dialogRef.current);
+    dialogRef.current.showModal();
+  };
   return (
     <>
+      <button onClick={modal} id="openModal">
+        Open the modal
+      </button>
+      <Modal dialogRef={dialogRef} />
       <div>X Position: {x}</div>
       <div>Y Position: {y}</div>
       <button onClick={toggle}>Toggle</button>
